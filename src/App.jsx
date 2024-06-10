6241b556930b0c974fc2488e9f7bfa14c468494d
Ghozi Izzulhaq <ghozihaq2017@gmail.com>
2024-06-10 14:26:47 +0700

    Develop add todo feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable no-unused-vars */
 import React, { useState } from 'react';
 import Todos from './components/Todos';
+import TodoForm from './components/TodoForm';
 
 function App() {
   const [todos, setTodos] = useState([
@@ -24,26 +25,41 @@ function App() {
   const toggleCompleted = (todoId) => {
     const updatedTodos = todos.map((todo) => {
       if (todo.id === todoId) {
-        todo.completed = !todo.completed
+        todo.completed = !todo.completed;
       }
-      return todo
-    })
-    setTodos(updatedTodos)
-  }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  };
 
   const deleteTodo = (todoId) => {
     const deleteTodo = todos.filter((todo) => {
       if (todo.id != todoId) {
-        return todo
+        return todo;
       }
-    })
-    setTodos(deleteTodo)
+    });
+    setTodos(deleteTodo);
+  };
+
+  const addTodo = (todoTitle) => {
+    if (todoTitle === '') {
+      return
+    }
+
+    const newTodo = {
+      id: todos.length + 1,
+      title: todoTitle,
+      completed: false,
+    }
+
+    const updatedTodos = todos.concat(newTodo)
+    setTodos(updatedTodos)
   }
 
   return (
     <div style={styles.container}>
       <h1 style={styles.title}>My Todo List</h1>
-      {/* Teruskan function toggleCompleted ke component Todos */}
+      <TodoForm addTodo={addTodo} />
       <Todos todos={todos} toggleCompleted={toggleCompleted} deleteTodo={deleteTodo} />
     </div>
   );
